3c162684f56aa4044ec68f835bd4f9699aaf9ad5
yuto14prog <yuto14prog@gmail.com>
2022-03-06 23:10:03 +0900

    feat: :sparkles: Footer.jsをadd

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function Footer({ words, home }) {
+  return (
+    <footer className='py-7 text-center'>
+      {home ? (
+        <p>{words}</p>
+      ) : (
+        <Link href='/'>
+          <a>{words}</a>
+        </Link>
+      )}
+    </footer>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Footer from "../components/Footer";
 import HomeHeader from "../components/homeheader";
 
 export default function Home() {
   return (
     <>
       <HomeHeader />
+      <Footer home words='ver. 1.0.0' />
     </>
   )
 }
