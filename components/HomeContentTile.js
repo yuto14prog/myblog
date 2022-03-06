d0f05c8596e7dfc07f1c17c412f6f3c31a120a5a
yuto14prog <yuto14prog@gmail.com>
2022-03-06 23:19:49 +0900

    WIP: :construction: HomeContentTile.jsの雛形を修正

---
+++ components/HomeContentTile.js
@@ -0,0 +1,7 @@
+export default function HomeContentTile() {
+  return (
+    <div>
+      Enter
+    </div>
+  );
+}
--- components/HomeContentsGrid.js
+++ components/HomeContentsGrid.js
@@ -1,7 +1,9 @@
+import HomeContentTile from "./HomeContentTile";
+
 export default function HomeContentsGrid() {
   return (
     <main>
-
+      <HomeContentTile />
     </main>
   );
 }
