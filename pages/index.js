25995fc434802e124c6dd0abb0374ac58c065199
yuto14prog <yuto14prog@gmail.com>
2022-03-07 10:48:32 +0900

    WIP: :construction: index.jsにgetstaticpropsをadd

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import Footer from "../components/Footer";
 import HomeHeader from "../components/homeheader";
+import { client } from "../libs/client";
 
-export default function Home() {
+export default function Home({ blog }) {
   return (
     <>
       <HomeHeader />
+      <div>
+        <ul>
+          {blog.map((blog) => (
+            <li key={blog.id}>{blog.title}</li>
+          ))}
+        </ul>
+      </div>
       <Footer home words='ver. 1.0.0' />
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  return {
+    props: {
+      blog: data.contents
+    }
+  }
 }
