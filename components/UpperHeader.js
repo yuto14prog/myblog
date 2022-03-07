863427f3857f418eca5a695185a4d1dddcf4050c
yuto14prog <yuto14prog@gmail.com>
2022-03-07 21:06:45 +0900

    fix: :adhesive_bandage: UpperHeaderのLink忘れてた

--- components/UpperHeader.js
+++ components/UpperHeader.js
@@ -1,17 +1,22 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { name } from './HomeHeaderTitle';
 
 export default function UpperHeader() {
   return (
-    <header className='flex row items-center justify-center my-4'>
-      <Image
-        priority
-        src='/icon.png'
-        height={50}
-        width={50}
-        alt='アイコン'
-      />
-      <h1 className='ml-3 text-2xl font-title'>{name}</h1>
-    </header>
+    <Link href='/'>
+      <a className='text-text'>
+        <header className='flex row items-center justify-center my-4'>
+          <Image
+            priority
+            src='/icon.png'
+            height={50}
+            width={50}
+            alt='アイコン'
+          />
+          <h1 className='ml-3 text-2xl font-title'>{name}</h1>
+        </header>
+      </a>
+    </Link>
   );
 }
