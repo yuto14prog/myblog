9ee53e8c761e60cc13a27d6bced6cc076abee478
yuto14prog <yuto14prog@gmail.com>
2022-03-07 11:55:18 +0900

    feat: :sparkles: HomeContentTileの見た目だけ(cssなし)

--- components/HomeContentTile.js
+++ components/HomeContentTile.js
@@ -1,7 +1,26 @@
-export default function HomeContentTile() {
+import Image from 'next/image';
+
+export default function HomeContentTile({ heroImage, title, description, publishedAt }) {
   return (
     <div>
-      Enter
+      {heroImage ? (
+        <Image
+          src={heroImage.url}
+          alt='ヒーローイメージ'
+          width={320}
+          height={240}
+        />
+      ) : (
+        <Image
+          src='/dummy.png'
+          alt='ダミー画像'
+          width={320}
+          height={240}
+        />
+      )}
+      <h2>{title}</h2>
+      <h3>{description}</h3>
+      <h3>{publishedAt}</h3>
     </div>
   );
 }
