1c8f1e5975de870ef2041a1f46e6fe289afb6a0c
yuto14prog <yuto14prog@gmail.com>
2022-03-07 13:20:06 +0900

    UI: :art: HomeContentGridのスタイルを修正

--- components/HomeContentsGrid.js
+++ components/HomeContentsGrid.js
@@ -2,8 +2,8 @@ import HomeContentTile from "./HomeContentTile";
 
 export default function HomeContentsGrid({ content }) {
   return (
-    <main>
-      <ul>
+    <main className="py-20 bg-background">
+      <ul className="max-w-max mx-auto grid grid-cols-3 gap-10">
         {content.map((content) => (
           <li key={content.id}>
             <HomeContentTile heroImage={content.heroImage} title={content.title} description={content.description} publishedAt={content.publishedAt} />
