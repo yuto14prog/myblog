bc2abe589dbfd6fc36bda5d2567f8855b6a358a3
yuto14prog <yuto14prog@gmail.com>
2022-03-08 11:38:51 +0900

    fix: :adhesive_bandage: 不要なpropsが残ってた

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function Footer({ words, home }) {
+export default function Footer({ home }) {
   return (
     <footer className='py-7 text-center'>
       {home ? (
