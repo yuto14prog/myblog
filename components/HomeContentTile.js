9becfbdd1157467d214f3c16eb76d40e04736423
yuto14prog <yuto14prog@gmail.com>
2022-03-08 10:25:07 +0900

    UI: :art: ...省略

--- components/HomeContentTile.js
+++ components/HomeContentTile.js
@@ -25,8 +25,8 @@ export default function HomeContentTile({ heroImage, title, description, publish
         />
       )}
       <div className='py-2 px-4 text-text'>
-        <h2 className='text-2xl'>{title}</h2>
-        <p className='text-base my-3'>{description}</p>
+        <h2 className='text-2xl overflow-hidden text-ellipsis whitespace-nowrap'>{title}</h2>
+        <p className='text-base my-3 overflow-hidden text-ellipsis whitespace-nowrap'>{description}</p>
         <p className='text-base text-right'>{dayjs.utc(publishedAt).tz('Asia/Tokyo').format('YYYY/MM/DD')}</p>
       </div>
     </div>
