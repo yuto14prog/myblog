f97efbf3ed3cf182cce711b167bfe9878b66cabf
yuto14prog <yuto14prog@gmail.com>
2022-03-08 10:56:55 +0900

    UI: :art: アイコン消した

--- components/HomeHeaderTitle.js
+++ components/HomeHeaderTitle.js
@@ -4,14 +4,7 @@ export const name = "yuto14prog"
 
 export default function HomeHeaderTitle() {
   return (
-    <div className='flex flex-col items-center pt-16'>
-      <Image
-        priority
-        src='/icon.png'
-        height={190}
-        width={190}
-        alt='アイコン'
-      />
+    <div className='flex flex-col items-center pt-14'>
       <h1 className='text-5xl mt-5 mb-16 font-title'>{name}</h1>
     </div>
   );
