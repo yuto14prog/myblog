1e0809663750e8cf7793cf15cfe3369ae7fad31e
yuto14prog <yuto14prog@gmail.com>
2022-03-10 17:39:35 +0900

    fix: :adhesive_bandage: client.tsの型をかたした

--- libs/client.ts
+++ libs/client.ts
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: 'mmsk-blog',
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.API_KEY || '',
 })
