6c3036afe0fbc0b9b98ba914559d72b76f6c7381
yuto14prog <yuto14prog@gmail.com>
2022-03-25 05:03:10 +0900

    fix: :adhesive_bandage: [id]の型をかたした

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,7 +1,13 @@
+import { GetStaticProps } from "next";
 import { Footer, Header, BlogArticleContent, Layout } from "../../components/index";
 import { client } from "../../libs/client";
+import { Blog } from "../../types/blog";
 
-export default function BlogArticlePage({ content }) {
+type Props = {
+  content: Blog
+}
+
+export default function BlogArticlePage({ content }: Props) {
   return (
     <Layout>
       <Header />
@@ -11,14 +17,24 @@ export default function BlogArticlePage({ content }) {
   );
 }
 
+const toStringId = (value: string | string[] | undefined): string => {
+  if (!value) {
+    throw new Error("Error: ID is undefined");
+  }
+  if (Array.isArray(value)) {
+    return value[0]
+  }
+  return value
+}
+
 export const getStaticPaths = async () => {
   const data = await client.get({ endpoint: 'blog' })
-  const paths = data.contents.map((content) => `/blog/${content.id}`)
+  const paths = data.contents.map((content: Blog) => `/blog/${content.id}`)
   return { paths, fallback: false }
 }
 
-export const getStaticProps = async (context) => {
-  const id = context.params.id
+export const getStaticProps: GetStaticProps = async (context) => {
+  const id = toStringId(context.params?.id)
   const data = await client.get({ endpoint: 'blog', contentId: id })
   return {
     props: {
