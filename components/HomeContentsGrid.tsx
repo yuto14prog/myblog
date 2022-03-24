8b8259284488d5c83adcbdff17a971bba877a55b
yuto14prog <yuto14prog@gmail.com>
2022-03-25 05:01:53 +0900

    fix: :adhesive_bandage: HomeContentsGridの型をかたした

--- components/HomeContentsGrid.tsx
+++ components/HomeContentsGrid.tsx
@@ -1,7 +1,12 @@
 import { HomeContentTile } from "./index";
 import Link from 'next/link';
+import { Blog } from '../types/blog';
 
-export default function HomeContentsGrid({ content }) {
+type Props = {
+  content: Blog[]
+}
+
+export default function HomeContentsGrid({ content }: Props) {
   return (
     <main className="py-20 bg-background">
       <ul className="max-w-max mx-auto grid lg:grid-cols-3 md:grid-cols-2 lg:gap-10 md:gap-9 grid-cols-1 gap-7 font-mplus">
